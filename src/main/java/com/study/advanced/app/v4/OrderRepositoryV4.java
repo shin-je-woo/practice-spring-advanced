270afc3295800df821c08bf605f009c44dba5d63
shin.jewoo <shinjw0926@naver.com>
2023-08-01 23:03:22 +0900

    [템플릿 메서드 패턴과 콜백 패턴] 템플릿 메서드 패턴 - 적용

---
+++ src/main/java/com/study/advanced/app/v4/OrderRepositoryV4.java
@@ -0,0 +1,37 @@
+package com.study.advanced.app.v4;
+
+import com.study.advanced.trace.logtrace.LogTrace;
+import com.study.advanced.trace.template.AbstractTemplate;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepositoryV4 {
+
+    private final LogTrace trace;
+
+    public void save(String itemId) {
+
+        AbstractTemplate<Void> template = new AbstractTemplate<>(trace) {
+            @Override
+            protected Void call() {
+                if (itemId.equals("ex")) {
+                    throw new IllegalStateException("예외 발생!!");
+                }
+                sleep(1000);
+                return null;
+            }
+        };
+        template.execute("OrderRepository.save");
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
