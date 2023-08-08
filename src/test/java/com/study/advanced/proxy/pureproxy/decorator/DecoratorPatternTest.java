01dfb30990d400cc4dcb52839a285d32f2a1653e
shin.jewoo <shinjw0926@naver.com>
2023-08-08 22:15:36 +0900

    [프록시 패턴과 데코레이터 패턴] 데코레이터 패턴 예제

---
+++ src/test/java/com/study/advanced/proxy/pureproxy/decorator/DecoratorPatternTest.java
@@ -0,0 +1,39 @@
+package com.study.advanced.proxy.pureproxy.decorator;
+
+import com.study.advanced.proxy.pureproxy.decorator.code.DecoratorPatternClient;
+import com.study.advanced.proxy.pureproxy.decorator.code.MessageDecorator;
+import com.study.advanced.proxy.pureproxy.decorator.code.RealComponent;
+import com.study.advanced.proxy.pureproxy.decorator.code.TimeDecorator;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class DecoratorPatternTest {
+
+    @Test
+    void noDecorator() {
+        RealComponent realComponent = new RealComponent();
+        DecoratorPatternClient client = new DecoratorPatternClient(realComponent);
+        client.execute();
+    }
+
+    @Test
+    void decorator1() {
+        RealComponent realComponent = new RealComponent();
+        MessageDecorator messageDecorator = new MessageDecorator(realComponent);
+        DecoratorPatternClient client = new DecoratorPatternClient(messageDecorator);
+        client.execute();
+    }
+
+    /**
+     * 프록시 연쇄
+     */
+    @Test
+    void decorator2() {
+        RealComponent realComponent = new RealComponent();
+        MessageDecorator messageDecorator = new MessageDecorator(realComponent);
+        TimeDecorator timeDecorator = new TimeDecorator(messageDecorator);
+        DecoratorPatternClient client = new DecoratorPatternClient(timeDecorator);
+        client.execute();
+    }
+}
