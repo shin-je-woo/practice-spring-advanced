b6c4fd98606c552061806a41321635bc7fb74b9e
shin.jewoo <shinjw0926@naver.com>
2023-08-12 18:54:20 +0900

    [스프링이 지원하는 프록시] 프록시 팩토리 - 적용

--- src/main/java/com/study/advanced/AdvancedApplication.java
+++ src/main/java/com/study/advanced/AdvancedApplication.java
@@ -1,6 +1,6 @@
 package com.study.advanced;
 
-import com.study.advanced.proxy.config.v2_dynamicproxy.DynamicProxyFilterConfig;
+import com.study.advanced.proxy.config.v3_proxyfactory.ProxyFactoryConfigV2;
 import com.study.advanced.trace.logtrace.LogTrace;
 import com.study.advanced.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -13,7 +13,9 @@ import org.springframework.context.annotation.Import;
 //@Import(InterfaceProxyConfig.class)
 //@Import(ConcreteProxyConfig.class)
 //@Import(DynamicProxyBasicConfig.class)
-@Import(DynamicProxyFilterConfig.class)
+//@Import(DynamicProxyFilterConfig.class)
+//@Import(ProxyFactoryConfigV1.class)
+@Import(ProxyFactoryConfigV2.class)
 @SpringBootApplication(scanBasePackages = "com.study.advanced.proxy.app")
 public class AdvancedApplication {
 
