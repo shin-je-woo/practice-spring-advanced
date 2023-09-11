e1a7188dccf259003089a0ef60e5ed7f3bf083fc
shin.jewoo <shinjw0926@naver.com>
2023-09-11 21:29:15 +0900

    [스프링 AOP 구현] 스프링 AOP 구현1 - 시작

--- src/test/java/com/study/advanced/aop/AopTest.java
+++ src/test/java/com/study/advanced/aop/AopTest.java
@@ -2,15 +2,18 @@ package com.study.advanced.aop;
 
 import com.study.advanced.aop.order.OrderRepository;
 import com.study.advanced.aop.order.OrderService;
+import com.study.advanced.aop.order.aop.AspectV1;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.aop.support.AopUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 @Slf4j
 @SpringBootTest
+@Import(AspectV1.class)
 public class AopTest {
 
     @Autowired
