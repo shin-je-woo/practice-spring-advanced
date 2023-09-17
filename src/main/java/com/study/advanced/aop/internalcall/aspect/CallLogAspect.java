b07e5f7872944ff4fab6d5e4ee49a3c0ceef1c02
shin.jewoo <shinjw0926@naver.com>
2023-09-17 17:20:38 +0900

    [스프링 AOP - 실무 주의사항] 프록시와 내부 호출 - 문제와 3가지 대안(자기 자신 주입, 지연 조회, 구조 변경)

---
+++ src/main/java/com/study/advanced/aop/internalcall/aspect/CallLogAspect.java
@@ -0,0 +1,16 @@
+package com.study.advanced.aop.internalcall.aspect;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+
+@Slf4j
+@Aspect
+public class CallLogAspect {
+
+    @Before("execution(* com.study.advanced.aop.internalcall..*.*(..))")
+    public void doLog(JoinPoint joinPoint) {
+        log.info("aop={}", joinPoint.getSignature());
+    }
+}
