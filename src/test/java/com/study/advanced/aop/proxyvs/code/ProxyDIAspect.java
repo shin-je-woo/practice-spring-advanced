9ec4757721217c3449b3fc544ebf978748f82a39
shin.jewoo <shinjw0926@naver.com>
2023-09-17 18:34:50 +0900

    [스프링 AOP - 실무 주의사항] 프록시 기술과 한계 - 타입 캐스팅, 의존관계 주입

---
+++ src/test/java/com/study/advanced/aop/proxyvs/code/ProxyDIAspect.java
@@ -0,0 +1,16 @@
+package com.study.advanced.aop.proxyvs.code;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+
+@Slf4j
+@Aspect
+public class ProxyDIAspect {
+
+    @Before("execution(* com.study.advanced.aop..*.*(..))")
+    public void doTrace(JoinPoint joinPoint) {
+        log.info("[proxyDIAdvice] {}", joinPoint.getSignature());
+    }
+}
